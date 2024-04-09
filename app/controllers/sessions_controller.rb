ffdeeb555b3bdb2c1bf8767b7398b3369a636489
ryuuya <ryuuya0921@gmail.com>
2024-04-10 08:28:23 +0900

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-
+  
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "ログインに成功しました"
       redirect_to users_path  # ログイン後に遷移するページを指定する
     else
-      flash.now[:alert] = "ログインに失敗しました"
+      flash.now[:alert] = "メールアドレスまたはパスワードが間違っています"
       render :new
     end
   end
+  
 
   def destroy
   end
