44dfa4ac37de9a2ef99c73fd608b8ae5d0a82ad6
ryuuya <ryuuya0921@gmail.com>
2024-04-10 08:15:19 +0900

    add:ログイン機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "ログインに成功しました"
+      redirect_to users_path  # ログイン後に遷移するページを指定する
+    else
+      flash.now[:alert] = "ログインに失敗しました"
+      render :new
+    end
   end
 
   def destroy
