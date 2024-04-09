1c4c711a8f2306bd05ecabe7e90251de942d61db
ryuuya <ryuuya0921@gmail.com>
2024-04-10 08:49:43 +0900

    add:各ユーザーに推移

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "ログインに成功しました"
-      redirect_to users_path  # ログイン後に遷移するページを指定する
+      redirect_to users_path(user)  # ログイン後に遷移するページを指定する
     else
       flash.now[:alert] = "メールアドレスまたはパスワードが間違っています"
       render :new
@@ -17,5 +17,11 @@ class SessionsController < ApplicationController
   
 
   def destroy
+  end
+
+  private
+  
+  def current_user #セッションに保存されたユーザーIDを使用してログイン中のユーザーを特定
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-
+  before_action :require_login, only: [:show]
   def index
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -27,6 +31,13 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def require_login
+    unless logged_in?
+      flash[:alert] = "ログインしてください"
+      redirect_to login_path
+    end
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
