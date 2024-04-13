f83087d31e3da8ec6f8e77cda49fafc525c7e3a9
ryuuya <ryuuya0921@gmail.com>
2024-04-13 11:01:57 +0900

    update:修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,14 @@ Rails.application.routes.draw do
     member do
       get 'account_settings' # アカウント設定ページ
       get 'profile_settings' # プロフィール設定ページ
+      get 'edit' # 編集ページ
+      patch 'account_settings', to: 'users#update_account_settings'
     end
   end
+  resources :users
+  get '/users/new', to: 'users#new' # 新規登録ページへのルートを追加
 
-  #Sessions
+  # Sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy' # ログアウトのルートを追加
