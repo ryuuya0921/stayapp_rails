8333efb55ad84da9cb3d9ec0dd894186d953728e
ryuuya <ryuuya0921@gmail.com>
2024-04-19 11:26:17 +0900

    add:施設新規登録作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,7 @@
 class RoomsController < ApplicationController
 
   before_action :require_login, except: [:index, :show]
-  
+
   def index
     @rooms = Room.all
   end
@@ -13,4 +13,22 @@ class RoomsController < ApplicationController
   def show
     @room  = Room.find(params[:id])
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      # 保存が成功した場合の処理
+      redirect_to @room, notice: "施設が正常に登録されました。"
+    else
+        # 保存が失敗した場合の処理
+      render 'new'
+    end
+  end
+  
+    private
+  
+  def room_params
+    params.require(:room).permit(:name)
+  end
+  
 end
