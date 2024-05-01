54909c0936af957b238780bdb00113096322cf07
ryuuya <ryuuya0921@gmail.com>
2024-05-01 21:38:57 +0900

    delete

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,11 +6,8 @@ class ReservationsController < ApplicationController
 
   def confirm
     @room = Room.find(params[:reservation][:room_id])  # 予約する部屋のIDからRoomオブジェクトを取得
-    logger.debug "Room loaded: #{@room.inspect}"
     @reservation = Reservation.new(reservation_params)
-    logger.debug "Reservation params: #{reservation_params.inspect}"
     @reservation.user = current_user
-    logger.debug "Current user: #{current_user.inspect}"  # ユーザー情報のデバッグ出力
   end
 
   def create
@@ -21,9 +18,7 @@ class ReservationsController < ApplicationController
 
     if @reservation.save
       redirect_to reservations_path, notice: '予約が確定されました。'  # 成功時はその部屋の詳細ページへリダイレクト
-      logger.debug "Reservation saved successfully."
     else
-      logger.debug "Failed to save reservation: #{@reservation.errors.full_messages}"
       flash[:notice] = '予約が失敗しました。'
       render "rooms/show", status: :unprocessable_entity  # 失敗時は部屋の詳細ページを再表示
     end
