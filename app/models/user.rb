19b4e23d1aea22ba1c0a9d233af3953e2f6e4e39
ryuuya <ryuuya0921@gmail.com>
2024-05-04 04:44:46 +0900

    add:password修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   #パスワードを6文字以上の設定
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_blank: true
 
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Active Storageを使用したアバター画像の添付
   has_one_attached :avatar
 end
