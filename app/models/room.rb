c36ec602a27bbdbdc89434f5cce7761c36a6c0e6
ryuuya <ryuuya0921@gmail.com>
2024-05-18 22:26:07 +0900

    add:price0

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   #料金が0円未満の場合はモデルが無効になり、データベースに保存されなくなる
-  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, numericality: { greater_than: 0, message: "は0円より大きい必要があります" }
   has_one_attached :image
   has_many :reservations, dependent: :destroy
 end
