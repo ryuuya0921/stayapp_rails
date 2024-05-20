b3bd0aa7c1dd4e860511035dad2070520c395ebe
ryuuya <ryuuya0921@gmail.com>
2024-05-20 23:41:41 +0900

    update:price

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,9 @@ class Room < ApplicationRecord
   validates :price, numericality: { greater_than: 0, message: "は0円より大きい必要があります" }
   has_one_attached :image
   has_many :reservations, dependent: :destroy
+
+    def total_price
+      # 予約がある場合は、その合計金額を返す
+      reservations.sum(&:total_price)
+    end
 end
