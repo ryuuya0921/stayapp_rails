d3b071365dd248b1505e60daf2b7fc15d05db9df
ryuuya <ryuuya0921@gmail.com>
2024-05-26 15:23:20 +0900

    add:validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
   #料金が0円未満の場合はモデルが無効になり、データベースに保存されなくなる
   validates :price, numericality: { greater_than: 0, message: "は0円より大きい必要があります" }
   validates :address, presence: { message: "を入力してください" } # 住所の必須バリデーション
+  validates :description, presence: { message: "を入力してください" }
+  validates :name, presence: { message: "を入力してください" }
   has_one_attached :image
   has_many :reservations, dependent: :destroy
 
